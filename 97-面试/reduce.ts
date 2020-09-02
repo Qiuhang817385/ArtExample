2710c000b5392dda38f4cec9fac8e83f6acf43ff
Qiuhang817385 <826266129@qq.com>
2020-09-02 23:36:22 +0800

    周三 9月 02 11:35 下午

---
+++ 97-面试/reduce.ts
@@ -0,0 +1,10 @@
+// 只有第一次用到了一下
+function fizzBuzzReducer(acc, element) {
+  if (element % 15 === 0) return `${acc}Fizz Buzz\n`
+  if (element % 5 === 0) return `${acc}Fizz\n`
+  if (element % 3 === 0) return `${acc}Buzz\n`
+  return `${acc}${element}\n`
+  // 展示
+}
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+console.log(nums.reduce(fizzBuzzReducer, '1s'))
