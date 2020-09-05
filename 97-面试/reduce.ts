bc75effdeabcf2132474245ee32d33ffac018dc8
Qiuhang817385 <826266129@qq.com>
2020-09-05 23:03:05 +0800

    周六 9月 05 11:02

--- 97-面试/reduce.ts
+++ 97-面试/reduce.ts
@@ -4,6 +4,7 @@ function fizzBuzzReducer(acc, element) {
   if (element % 5 === 0) return `${acc}Fizz\n`
   if (element % 3 === 0) return `${acc}Buzz\n`
   return `${acc}${element}\n`
+  // 日常打开
   // 展示
 }
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
