83a7522da7563a768fb2f2f6f001c1b17ba6a586
Qiuhang817385 <826266129@qq.com>
2020-09-06 23:32:02 +0800

    周日 9月 06

--- 97-面试/reduce.ts
+++ 97-面试/reduce.ts
@@ -6,6 +6,7 @@ function fizzBuzzReducer(acc, element) {
   return `${acc}${element}\n`
   // 日常打开
   // 展示
+  // 打卡
 }
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 console.log(nums.reduce(fizzBuzzReducer, '1s'))
